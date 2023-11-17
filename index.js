36cb9e7ac810b271fdae3d3f4e1fb0b58e130b70
KatriLindvall1 <lindvallkatri@hotmail.com>
2023-11-17 10:21:19 +0200

    cors

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(express.static('dist'))
 
 
