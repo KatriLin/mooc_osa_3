c7700bbe013e3d2071b23d56d0450b3989874243
KatriLindvall1 <lindvallkatri@hotmail.com>
2023-11-17 10:41:29 +0200

    added error handling

--- index.js
+++ index.js
@@ -98,6 +98,12 @@ app.post('/api/persons',(request,response) => {
 
   response.json(person)
 })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
