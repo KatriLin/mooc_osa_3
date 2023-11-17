0385d82448f954bb835b1a4d2c66481876686d08
KatriLindvall1 <lindvallkatri@hotmail.com>
2023-11-17 10:15:51 +0200

    added express middleware

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
-
 const morgan = require('morgan')
 const cors = require('cors')
+const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 
 
