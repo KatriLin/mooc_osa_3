47918e333368b3286ba04d47e5feda4541af0046
KatriLindvall1 <lindvallkatri@hotmail.com>
2023-11-24 10:44:34 +0200

    exercises  3.15.-3.18.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 app.use(cors())
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 const Person = require('./models/person')
 
 
@@ -27,35 +27,34 @@ app.get('/api/persons', (request,response) => {
     })
 })
 
-app.delete('/api/persons/:id',(request,response) => {
-  const id = Number(request.params.id)
-  const updatedPersons = persons.filter(person => person.id !== id);
-  persons = updatedPersons;
-  response.status(204).end()
+app.delete('/api/persons/:id',(request,response, next) => {
+  Person.findByIdAndDelete(request.params.id).then(result => {
+    response.status(204).end()
+  }).catch(error => next(error))
 })
 
 
 
 
 app.get('/info', (request,response) => {
-  const date_time = new Date();
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-  const personscount = persons.length;
-  const info = `Phonebook has info for ${personscount} people`;
-  const time = `${date_time.toDateString()} ${date_time.toTimeString()} ${timeZone}  `
-  const fullInfo = `${info}<br>${time}`;
-  response.send(fullInfo);
+  Person.find({}).then(persons => {
+  const info = `<p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>`
+  response.send(info);
+ 
+  })
 })
 
-app.get('/api/persons/:id',(request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id',(request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if(person){
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
+
 
 app.post('/api/persons',(request,response) => {
   const body = request.body
@@ -74,11 +73,26 @@ app.post('/api/persons',(request,response) => {
     response.json(savedPerson)
   })
 })
+
+app.put('/api/persons/:id',(request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
+})
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
