95a794155fc386d6a360b53099dfc0df4d413fa9
KatriLindvall1 <lindvallkatri@hotmail.com>
2023-11-30 12:57:29 +0200

    error handling

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ return ' '
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
 
 
 app.get('/api/persons', (request,response,next) => {
@@ -64,6 +74,7 @@ app.post('/api/persons',(request,response, next) => {
     return response.status(400).json({
       error: "Name or/and number is missing"
     })
+    .catch(error => next(error))
   }
  
 
@@ -75,7 +86,7 @@ app.post('/api/persons',(request,response, next) => {
     response.json(savedPerson)
   })
   .catch(error => next(error))
-  console.log(error.response.data)
+ 
 })
 
 app.put('/api/persons/:id',(request, response, next) => {
@@ -96,7 +107,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
-
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
--- mongo.js
+++ mongo.js
@@ -15,8 +15,22 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type : String, 
+    minlength : 8,
+    validate : {
+      validator : function(v) {
+        return /^\d{2,3}-\d{7,}/.test(v)
+      },
+      message : props => `${props.value} is not a valid phone number!`
+    },
+    required : true
+  },
 })
 
 const Person = mongoose.model('Person', personSchema)
